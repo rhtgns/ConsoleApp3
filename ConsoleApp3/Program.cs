8d5c2a8a7a0e41d512af7b50084ef5218921cc51
Rohat Güneş <rohatgns44@gmail.com>
2024-07-19 05:19:10 +1000

    homework 2

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" rohat gunes ");
+using System;
 
-Console.WriteLine( "patika uzman kadrosu ve tecrübeli eğitmenleri sayeisnde yazilim dünyasına hızlı bir şekilde yapabileceğim iöin bootcampteyim     ");
+Console.WriteLine(" lutfen asagıdakileri bilgileri giriniz: ");
+int tc = 1234567890;
+int ilkUrun = 250;
+int ikinciUrun = 300;
+long tc = 123456789;
+long telNo = 5554443322;
+
+
+Console.WriteLine($"tc kimlik numaranız : {tc} "  );
+
+Console.WriteLine("adı");
+Console.ReadLine();
+
+Console.Write("Soyad:");
+Console.ReadLine();
+
+Console.WriteLine("telefon numarası");
+Console.ReadLine();
+
+Console.WriteLine("yas");
+Console.ReadLine();
+
+Console.WriteLine("ilk aldığı ürünğn fiyatı ");
+Console.ReadLine($"{ ilkUrun+ikinciUrun} harcama karsılıgı kazanılamn 10% patika puan miktarı -");
+
+Console.WriteLine("ikinci aldığı ürünğn fiyatı ");
+Console.ReadLine();
+
+Console.WriteLine($"{tc} isimli ahmet yılmaz isimli kisi için kayıt oluşturulmuştur");
+Console.WriteLine($"{telNo} numarasına bilfirim mesajı gönderilmsitir ");
+Console.WriteLine($"");// bu kısmı yazamadım malasef //
+
+
+
+
+
+
