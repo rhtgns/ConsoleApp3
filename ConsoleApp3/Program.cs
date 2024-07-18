acf1115a379302b879e004f10ab1229b8a2ba097
rohat <rohat@ROHATGUNES>
2024-07-19 01:36:51 +1000

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" rohat gunes ");
+
+Console.WriteLine( "patika uzman kadrosu ve tecrübeli eğitmenleri sayeisnde yazilim dünyasına hızlı bir şekilde yapabileceğim iöin bootcampteyim     ");
